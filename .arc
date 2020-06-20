56e57413e042fa01ebecc52f3608df74f95d0c35
Kurt Kemple <kurtiskemple@gmail.com>
2020-06-20 17:04:25 -0400

    add function for self defined app

--- .arc
+++ .arc
@@ -3,3 +3,4 @@ functions
 
 @http
 post /update-apollo-twitch-channel
+get /self-defined-app-search
---
+++ src/http/get-self_defined_app_search/index.js
@@ -0,0 +1,38 @@
+const lunr = require("lunr");
+
+const definitions = require("./data.json");
+
+// create a map based on title for faster retrieval in search results
+const definitionMap = definitions.reduce((map, definition) => {
+  map[definition.title] = definition;
+  return map;
+}, {});
+
+// create the full text search
+const idx = lunr(function () {
+  this.ref("title");
+  this.field("title");
+  this.field("subterms");
+  this.field("content");
+
+  // load definitions into lunr index
+  for (definition in definitions) {
+    this.add(definitions[definition]);
+  }
+});
+
+exports.handler = async function http({ queryStringParameters }) {
+  if (!queryStringParameters) {
+    return {
+      statusCode: 400,
+    };
+  }
+
+  const matches = idx.search(queryStringParameters.q);
+  const results = matches.map((match) => definitionMap[match.ref]);
+
+  return {
+    body: JSON.stringify(results),
+    statusCode: 200,
+  };
+};
