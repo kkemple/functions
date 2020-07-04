b4f8cb7611c67c54e8e375153513a935001a82f8
Kurt Kemple <kurtiskemple@gmail.com>
2020-07-04 15:35:42 -0400

    remove static data, request from selfdefined app

--- src/http/get-self_defined_app_search/index.js
+++ src/http/get-self_defined_app_search/index.js
@@ -1,32 +1,41 @@
 const arc = require("@architect/functions");
 const lunr = require("lunr");
+const { default: axios } = require("axios");
 
-const definitions = require("./data.json");
-
-// create a map based on title for faster retrieval in search results
-const definitionMap = definitions.reduce((map, definition) => {
-  map[definition.title] = definition;
-  return map;
-}, {});
-
-// create the full text search
-const idx = lunr(function () {
-  this.ref("title");
-  this.field("title");
-  this.field("subterms");
-  this.field("content");
-
-  // load definitions into lunr index
-  for (definition in definitions) {
-    this.add(definitions[definition]);
-  }
-});
+let idx;
 
 async function http({ queryStringParameters }) {
+  let definitionMap;
+
   if (!queryStringParameters) {
     return {
       statusCode: 400,
     };
+  }
+
+  if (!idx) {
+    const { data: definitions } = await axios.get(
+      "https://deploy-preview-213--selfdefined.netlify.app/search.json"
+    );
+
+    // create a map based on title for faster retrieval in search results
+    definitionMap = definitions.reduce((map, definition) => {
+      map[definition.title] = definition;
+      return map;
+    }, {});
+
+    // create the full text search
+    idx = lunr(function () {
+      this.ref("title");
+      this.field("title");
+      this.field("subterms");
+      this.field("content");
+
+      // load definitions into lunr index
+      for (definition in definitions) {
+        this.add(definitions[definition]);
+      }
+    });
   }
 
   const matches = idx.search(queryStringParameters.q);
