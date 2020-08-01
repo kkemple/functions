880b396ffe83ba25ea565478cab828570daa26cf
Kurt Kemple <kurtiskemple@gmail.com>
2020-08-01 15:33:14 -0400

    add try catch for function (self defined)

--- src/http/get-self_defined_app_search/index.js
+++ src/http/get-self_defined_app_search/index.js
@@ -2,30 +2,26 @@ const arc = require("@architect/functions");
 const lunr = require("lunr");
 const { default: axios } = require("axios");
 
-let idx;
-
 async function http({ queryStringParameters }) {
-  let definitionMap;
+  try {
+    if (!queryStringParameters) {
+      return {
+        statusCode: 400,
+      };
+    }
 
-  if (!queryStringParameters) {
-    return {
-      statusCode: 400,
-    };
-  }
-
-  if (!idx) {
     const { data: definitions } = await axios.get(
       "https://deploy-preview-213--selfdefined.netlify.app/search.json"
     );
 
     // create a map based on title for faster retrieval in search results
-    definitionMap = definitions.reduce((map, definition) => {
+    const definitionMap = definitions.reduce((map, definition) => {
       map[definition.title] = definition;
       return map;
     }, {});
 
     // create the full text search
-    idx = lunr(function () {
+    const idx = lunr(function () {
       this.ref("title");
       this.field("title");
       this.field("subterms");
@@ -36,19 +32,18 @@ async function http({ queryStringParameters }) {
         this.add(definitions[definition]);
       }
     });
+
+    const matches = idx.search(queryStringParameters.q);
+    const results = matches.map((match) => definitionMap[match.ref]);
+
+    return {
+      body: JSON.stringify(results),
+      statusCode: 200,
+      cors: true,
+    };
+  } catch (error) {
+    console.log(error);
   }
-
-  const matches = idx.search(queryStringParameters.q);
-  const results = matches.map((match) => definitionMap[match.ref]);
-
-  return {
-    body: JSON.stringify(results),
-    statusCode: 200,
-    cors: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-  };
 }
 
 exports.handler = arc.http.async(http);
