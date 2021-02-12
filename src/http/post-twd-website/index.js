fc38ef917a7c9ae51b206adf7ed582193a7f158c
Kurt Kemple <kurtiskemple@gmail.com>
2021-02-12 18:58:25 -0500

    create server inside handler

--- src/http/post-twd-website/index.js
+++ src/http/post-twd-website/index.js
@@ -24,20 +24,21 @@ const schema = buildFederatedSchema([{ typeDefs, resolvers }]);
 const directives = { _publish: PublishDirective };
 SchemaDirectiveVisitor.visitSchemaDirectives(schema, directives);
 
-const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  key: process.env.PUSHER_KEY,
-  secret: process.env.PUSHER_SECRET,
-  cluster: process.env.PUSHER_CLUSTER,
-  useTLS: true,
-});
-
-let server = new ApolloServer({ schema, context: { pusher } });
-let handler = server.createHandler();
-
 exports.handler = function (event, context, callback) {
   let body = arc.http.helpers.bodyParser(event);
   // Body is now parsed, re-encode to JSON for Apollo
+
+  const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET,
+    cluster: process.env.PUSHER_CLUSTER,
+    useTLS: true,
+  });
+
+  let server = new ApolloServer({ schema, context: { pusher } });
+  let handler = server.createHandler();
+
   event.body = JSON.stringify(body);
   handler(event, context, callback);
 };
